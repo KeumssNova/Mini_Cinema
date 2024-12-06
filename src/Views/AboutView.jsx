9bcc62e3df80329b175ff001803fbe4ca91d1f52
Yanis <say.yanis91@gmail.com>
2024-12-06 21:36:32 +0100

    page about [YD]

--- src/Views/AboutView.jsx
+++ src/Views/AboutView.jsx
@@ -1,10 +1,14 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+export const img_300 = "https://image.tmdb.org/t/p/w300";
+export const unavailable =
+    "https://www.movienewz.com/img/films/poster-holder.jpg";
 
 
 const AboutView = () => {
 
     const { id } = useParams();
+    const [details, setDetails] = useState([]);
 
     useEffect(() => {
         const url = `https://api.themoviedb.org/3/movie/${id}`
@@ -18,18 +22,54 @@ const AboutView = () => {
 
         fetch(url, options)
             .then(res => res.json())
-            .then(json => console.log(json))
+            .then(json => setDetails(json))
             .catch(err => console.error(err));
+
+        console.log(url)
+
     }, [id])
 
-    // const { backdrop_path, name, title, overview };
+    console.log(details)
+
+
+    // const { backdrop_path, name, title, overview } = Val
 
     return (
-        <div>
-            
+        <div className='container-about'>
+            {details && ( // Check if details exists before accessing properties
+                <div className='card d-flex flex-row justify-content-center mt-3 gap-4'>
+                    <div className='card-image h-100'>
+                        <img
+                            src={
+                                // Check if details.backdrop_path exists and is a string
+                                typeof details.backdrop_path === 'string'
+                                    ? `${img_300}/${details.backdrop_path}`
+                                    : unavailable
+                            }
+                            alt={details.title || details.name}
+                            className=''
+                        />
+                    </div>
 
-        </div>
-    )
+                    <div className='card-body'>
+                        <h1 className=''> {details.title || details.name} </h1>
+                        <span>Release Date : {details.release_date}</span>
+                        <p><span>Overview</span> : {details.overview}</p>
+                    </div>
+                </div>
+
+            )
+            }
+        </div >
+    );
 }
 
 export default AboutView
+
+
+
+//     <div className=''>
+//     Release Date : {details.release_date}
+// </div>
+
+{/* <h1 className=''> {details.title || details.name} </h1> */ }
